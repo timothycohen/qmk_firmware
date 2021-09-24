0dda51079f228e623e16bc7d5a5e88949bbdb65a
timothycohen <tco.physics@gmail.com>
2021-09-24 19:54:17 -0400

    switched to LAYOUT instead of 4x12 due to a bug. works as expected now.

--- keyboards/keebio/nyquist/keymaps/timothycohen/keymap.c
+++ keyboards/keebio/nyquist/keymaps/timothycohen/keymap.c
@@ -1,6 +1,10 @@
 #include QMK_KEYBOARD_H
 
-// tco_v06
+
+// tco_v06. LAYOUT_ortho_4x12 wrote my right keyboard's top row to the 5th (and missing) bottom row of the left keyboard
+// something must be mismatched with #define LAYOUT_ortho_4x12
+// quick fixed by writing in the extra row of empty keys and switching from LAYOUT_ortho_4x12 to LAYOUT
+
 
 // Define layer names which are used in the keymap below
 #define _QWERTY 0
@@ -9,6 +13,7 @@
 #define _NAV 3
 #define _APPANDMEDIA 4
 #define _KEYBOARD 5
+
 
 //Using the = SCAN_RANGE tells the enum to start there
 enum custom_keycodes {
@@ -20,11 +25,10 @@ enum custom_keycodes {
 	KEYBOARD,
 };
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-
-
-[_QWERTY] = LAYOUT_ortho_4x12( \
+[_QWERTY] = LAYOUT( \
 	KC_ESC,							KC_Q,								KC_W,								KC_E,								KC_R,								KC_T,						\
 	KC_Y,								KC_U,								KC_I,								KC_O,								KC_P,								KC_BSPC,				\
 	
@@ -35,9 +39,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_N,								KC_M,								KC_COMM,						KC_DOT,							KC_SLSH,						RSFT_T(KC_1),		\
 	
 	LT(3,KC_CAPS),			KC_LCPO,						C_S_T(KC_MINS),			LALT_T(KC_LBRC),		LT(1,KC_DEL),				KC_ENT,					\
-	KC_SPC,							LT(2,KC_EQL),				RALT_T(KC_RBRC),		KC_LGUI,						KC_RCPC,						LT(3,KC_ENT)		),
+	KC_SPC,							LT(2,KC_EQL),				RALT_T(KC_RBRC),		KC_LGUI,						KC_RCPC,						LT(3,KC_ENT),		\
 
-[_SYMBOL] = LAYOUT_ortho_4x12( \
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO						),
+
+[_SYMBOL] = LAYOUT( \
 	KC_TILD,						KC_EXLM,						KC_AT,							KC_HASH,						KC_DLR,							KC_PERC,				\
 	KC_CIRC,						KC_AMPR,						KC_ASTR,						KC_LPRN,						KC_RPRN,						KC_TRNS,				\
 
@@ -48,9 +55,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_TRNS,				\
 
 	KC_NO,							KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS,				\
-	KC_TRNS,						MO(4),							KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS					),
+	KC_TRNS,						MO(4),							KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS,				\
 
-[_NUMBER] = LAYOUT_ortho_4x12( \
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO						),
+
+[_NUMBER] = LAYOUT( \
 	KC_GRV,							KC_1,								KC_2,								KC_3,								KC_4,								KC_5,						\
 	KC_6,								KC_7,								KC_8,								KC_9,								KC_0,								KC_TRNS,				\
 
@@ -61,9 +71,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_DOT,							KC_COMM,						KC_LT,							KC_GT,							KC_EQL,							KC_TRNS,				\
 
 	KC_NO,							KC_TRNS,						KC_TRNS,						KC_TRNS,						MO(4),							KC_TRNS,				\
-	KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS					),
+	KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS,				\
 
-[_NAV] = LAYOUT_ortho_4x12( \
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO						),
+
+[_NAV] = LAYOUT( \
 	KC_ESC,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
 	KC_HOME,						KC_PGDN,						KC_PGUP,						KC_END,							KC_NO,							KC_TRNS,				\
 
@@ -74,9 +87,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_WH_L,						KC_WH_D,						KC_WH_U,						KC_WH_R,						KC_NO,							KC_TRNS,				\
 
 	KC_NO,							KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_DEL,							KC_TRNS,				\
-	KC_TRNS,						KC_NO,							KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS					),
+	KC_TRNS,						KC_NO,							KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_TRNS,				\
 
-[_APPANDMEDIA] = LAYOUT_ortho_4x12( \
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO						),
+
+[_APPANDMEDIA] = LAYOUT( \
 	MO(5),							KC_MUTE,						KC_VOLD,						KC_VOLU,						KC_MPRV,						KC_MPLY,				\
 	KC_MNXT,						KC_NO,							KC_BRID,						KC_BRIU,						KC_LGUI,						KC_PSCR,				\
 
@@ -87,14 +103,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_F12,							KC_NO,							KC_NO,							KC_NO,							KC_LALT,						KC_NO,					\
 
 	KC_NO,							KC_TRNS,						KC_TRNS,						KC_TRNS,						KC_NO,							KC_TRNS,				\
-	KC_TRNS,						KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO						),
+	KC_TRNS,						KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
 
-[_KEYBOARD] = LAYOUT_ortho_4x12( \
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO						),
+
+[_KEYBOARD] = LAYOUT( \
 	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
 	KC_NO,							RGB_MOD,						RGB_VAI,						RGB_HUI,						RGB_SAI,						RGB_TOG,				\
 
 	RESET,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
 	KC_NO,							RGB_RMOD,						RGB_VAD,						RGB_HUD,						RGB_SAD,						KC_NO,					\
+
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
+	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
 
 	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
 	KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,							KC_NO,					\
